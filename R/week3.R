4ee59d99b24910ac998dbed2115b325949c79761
Ben Elliott <ellio747@umn.edu>
2026-02-03 19:59:36 -0600

    added Line 8-9, remove RA & attention failure

--- R/week3.R
+++ R/week3.R
@@ -5,3 +5,5 @@ setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 raw_df <- read.csv("data/week3.csv")
 raw_df$timeStart <- as.POSIXct(raw_df$timeStart, tz = "", "%Y-%m-%d %H:%M:%S")
 raw_df$timeEnd <- as.POSIXct(raw_df$timeEnd, tz = "", "%Y-%m-%d %H:%M:%S")
+clean_df <- subset(raw_df, subset = raw_df$timeEnd > "2017-06-30") #June 2017 was cutoff (RA = 6)
+clean_df <- subset(clean_df, subset = clean_df$q6 == 1) #removed 3 failed attention check
