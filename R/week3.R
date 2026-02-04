e68f3b487769007959aebc80bcb5ef14e0906bf3
Ben Elliott <ellio747@umn.edu>
2026-02-03 19:51:33 -0600

    Line 4-7 added

--- R/week3.R
+++ R/week3.R
@@ -1,2 +1,7 @@
 # Script Settings and Resources
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+
+# Data Import and Cleaning
+raw_df <- read.csv("data/week3.csv")
+raw_df$timeStart <- as.POSIXct(raw_df$timeStart, tz = "", "%Y-%m-%d %H:%M:%S")
+raw_df$timeEnd <- as.POSIXct(raw_df$timeEnd, tz = "", "%Y-%m-%d %H:%M:%S")
