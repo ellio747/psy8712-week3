75fd2f7216a51dfde410edef87960afa51012a60
Ben Elliott <ellio747@umn.edu>
2026-02-03 20:12:34 -0600

    added Line 12

--- R/week3.R
+++ R/week3.R
@@ -7,3 +7,6 @@ raw_df$timeStart <- as.POSIXct(raw_df$timeStart, tz = "", "%Y-%m-%d %H:%M:%S")
 raw_df$timeEnd <- as.POSIXct(raw_df$timeEnd, tz = "", "%Y-%m-%d %H:%M:%S")
 clean_df <- subset(raw_df, subset = raw_df$timeEnd > "2017-06-30") #June 2017 was cutoff (RA = 6)
 clean_df <- subset(clean_df, subset = clean_df$q6 == 1) #removed 3 failed attention check
+
+# Analysis
+clean_df$timeSpent <- difftime(time1 = clean_df$timeEnd, time2 = clean_df$timeStart, units = "secs")
