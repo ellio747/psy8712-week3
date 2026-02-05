68c48c87ec70f89cca7adcc2f5469ed4bd820d7c
Ben Elliott <ellio747@umn.edu>
2026-02-04 19:02:20 -0600

    fixed relative file convention added line 13 and 15-18

--- R/week3.R
+++ R/week3.R
@@ -2,7 +2,7 @@
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 
 # Data Import and Cleaning
-raw_df <- read.csv("data/week3.csv")
+raw_df <- read.csv("../data/week3.csv")
 raw_df$timeStart <- as.POSIXct(raw_df$timeStart, tz = "", "%Y-%m-%d %H:%M:%S")
 raw_df$timeEnd <- as.POSIXct(raw_df$timeEnd, tz = "", "%Y-%m-%d %H:%M:%S")
 clean_df <- subset(raw_df, subset = raw_df$timeEnd > "2017-06-30") #June 2017 was cutoff (RA = 6)
@@ -10,6 +10,9 @@ clean_df <- subset(clean_df, subset = clean_df$q6 == 1) #removed 3 failed attent
 
 # Analysis
 clean_df$timeSpent <- difftime(time1 = clean_df$timeEnd, time2 = clean_df$timeStart, units = "secs")
-# Need to go back to this analysis
+hist(as.numeric(clean_df$timeSpent)) #Was asked to recast timeSpent first; coerce to as.numeric as difftime variable type cannot go in hist()
 frequency_tables_list <- lapply(clean_df[,5:14], table)
 lapply(frequency_tables_list, barplot)
+sum((clean_df$q1 >= clean_df$q2) & (clean_df$q2 != clean_df$q3)) # sum when both conditions are TRUE
+for (i in frequency_tables_list) { barplot(i) 
+}
